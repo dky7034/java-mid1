461127370664f28efacea90f0e77662bf8304883
artp <dky7034@naver.com>
2025-10-27 14:22:31 +0900

    object

---
+++ src/lang/LangMain.java
@@ -0,0 +1,9 @@
+package lang;
+
+import java.lang.System; // 생략 허용 (기본 임포트)
+
+public class LangMain {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
